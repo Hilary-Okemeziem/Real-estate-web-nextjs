bff4fa78c9e6b0f785e834cf7c9ace53d77a4b5c
Hilary Okemeziem <hilaryokems@gmail.com>
2022-08-09 01:20:27 +0100

    production build

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get(url, {
         headers: {
-            'X-RapidAPI-Key': '297629cf2fmsh6fcc050ef398719p1fff89jsnffc3f065b113',
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         },
     });
